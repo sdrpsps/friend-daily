e05c1dd1355c5690f7aa5e1243659fd2abab52cf
sunny <sunny@bytespark.app>
2024-03-09 17:50:16 +0800

    style: 修改默认字体为 HarmonyOS Sans

--- components/Layout.vue
+++ components/Layout.vue
@@ -4,3 +4,16 @@
     <PostLayout />
   </div>
 </template>
+
+<style>
+@font-face {
+  font-family: HarmonyOS_Sans_SC_Regular;
+  font-style: normal;
+  font-display: swap;
+  src: url('https://static.bytespark.me/HarmonyOS_Sans_SC_Regular.woff2') format('woff2');
+}
+
+* {
+  font-family: HarmonyOS_Sans_SC_Regular, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}
+</style>
